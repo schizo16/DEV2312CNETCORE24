84c580427620b915363740b16b9d2d58ab2230a8
Batman <Batman@DESKTOP-6V9DUQE>
2024-01-17 00:29:37 +0700

    update

---
+++ Labguide6_3/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labguide6_3
+{
+    internal class Car
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+        public Car(string name, string color)
+        {
+            Name = name;
+            Color = color;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Car Name: {Name}");
+            Console.WriteLine($"Car Color: {Color}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Labguide6_3/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Labguide6_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // Tạo danh sách chứa 10 đối tượng Car
+            List<Car> carList = new List<Car>
+        {
+            new Car("Toyota", "Blue"),
+            new Car("Honda", "Red"),
+            new Car("Ford", "Green"),
+            new Car("Chevrolet", "Silver"),
+            new Car("BMW", "Black"),
+            new Car("Mercedes", "White"),
+            new Car("Audi", "Gray"),
+            new Car("Nissan", "Yellow"),
+            new Car("Hyundai", "Purple"),
+            new Car("Kia", "Orange")
+        };
+
+            // In danh sách trước khi xóa
+            Console.WriteLine("Danh sách trước khi xóa:");
+            foreach (Car car in carList)
+            {
+                car.DisplayInfo();
+            }
+
+            // Xóa các xe có màu đỏ
+            carList.RemoveAll(car => car.Color.ToLower() == "red");
+
+            // In danh sách sau khi xóa
+            Console.WriteLine("Danh sách sau khi xóa:");
+            foreach (Car car in carList)
+            {
+                car.DisplayInfo();
+            }
+
+            // Đợi người dùng nhấn phím Enter trước khi đóng chương trình
+            Console.ReadLine();
+        }
+    }
+}
